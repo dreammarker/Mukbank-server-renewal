dce0e97c765a41980549852b854be89eddb6e5bf
dreammarker <minjea456@naver.com>
2020-08-20 22:15:38 +0900

    restsearch api 작성
    검색시 해당 키워드를 포함한 식당 및 카페 음식점이 표시된다 자신의
    위치기점으로 검색된다

--- controller/restaurant/index.ts
+++ controller/restaurant/index.ts
@@ -3,10 +3,12 @@ const fdcategory = require('./fdcategory');
 const distancestend = require('./distancestend');
 const restdetail = require('./restdetail');
 const restlike = require('./restlike');
+const restsearch = require('./restsearch');
 export = {
   restdistance,
   fdcategory,
   distancestend,
   restdetail,
-  restlike
+  restlike,
+  restsearch
 };
