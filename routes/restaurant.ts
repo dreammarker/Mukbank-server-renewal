fbc7f3e6e879e79cb1a2a7b4679090fcc6429b1c
dreammarker <minjea456@naver.com>
2020-08-21 20:33:27 +0900

    카테고리 필터기능에 따른 api 작성 완료

--- routes/restaurant.ts
+++ routes/restaurant.ts
@@ -10,5 +10,6 @@ router.get('/category', restaurantController.fdcategory.get); //카테고리
 router.post('/detail', restaurantController.restdetail.post); //식당세부정보
 router.post('/restlike', restaurantController.restlike.post); //한식당의 like 횟수정보
 router.post('/restpaging', restaurantController.restdistance.paging); //paging
-router.post('/search',restaurantController.restsearch.post);
+router.post('/search',restaurantController.restsearch.post); //search 검색 
+router.post('/selectFilter',restaurantController.restfilter.post); // filter 기능
 module.exports = router;
